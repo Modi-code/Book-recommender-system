9941e1e376153ad3f48fe8f4f726f3515f3084d6
Mohamad <dyabmoh.md44@gmail.com>
2026-06-05 17:53:10 +0300

    Implement book recommendation logic and outcomes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import react, {Component,useState} from 'react';
+import {Component} from 'react';
 import data from './questions.json'
 import './Books.css'
 class App extends Component {
@@ -12,16 +12,19 @@ class App extends Component {
             option4:data[0].option4,
             number:data[1].number,
             check:false,
-            visible:true
+            visible:true,
+            answers: [],
+            recommendation: ""
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         document.body.style.backgroundColor='#fafcf4'
     }
-    handleChange = () => {
-        if(this.state.number<=4) {
+    handleChange = (selectedOption) => {
+        if(this.state.number <= 4) {
             this.setState({
                 check: true,
+                answers: [...this.state.answers, selectedOption],
                 question: data[this.state.number].question,
                 option1: data[this.state.number].option1,
                 option2: data[this.state.number].option2,
@@ -30,17 +33,38 @@ class App extends Component {
                 number: data[this.state.number].number + 1
             })
             setTimeout(() => {
-                    this.setState({check: false})
-                }
-                , 300)
-        }else if(this.state.number===5){
+                this.setState({check: false})
+            }, 300)
+        } else if(this.state.number === 5){
             this.setState({
-                number:this.state.number+1,
-                visible:false
+                answers: [...this.state.answers, selectedOption],
+                number: this.state.number + 1,
+                visible: false
+            }, () => {
+                // Scoring system logic
+                const counts = { 1: 0, 2: 0, 3: 0, 4: 0 };
+                this.state.answers.forEach(num => counts[num]++);
+
+                let maxOption = 1;
+                let maxCount = counts[1];
+                let isTie = false;
+
+                for (let i = 2; i <= 4; i++) {
+                    if (counts[i] > maxCount) { maxCount = counts[i]; maxOption = i; isTie = false; }
+                    else if (counts[i] === maxCount) { isTie = true; }
+                }
+
+                const books = {
+                    1: "'The Midnight Library' by Matt Haig",
+                    2: "'The Silent Patient' by Alex Michaelides",
+                    3: "'Sapiens' by Yuval Noah Harari",
+                    4: "'Atomic Habits' by James Clear"
+                };
+
+                this.setState({
+                    recommendation: isTie ? "'The Alchemist' by Paulo Coelho" : books[maxOption]
+                });
             })
-        }else{
-            console.log('daw')
-            this.handleSubmit()
         }
     }
     handleSubmit = () => {
@@ -56,52 +80,47 @@ class App extends Component {
                         <div className={`fade-in-elements-${this.state.check}`}>
                             {this.state.visible && <>
                                 <label className='question'>{question}</label>
+
                                 <label htmlFor='option-1'>
                                     <input
                                         type='checkbox'
                                         id='option-1'
                                         checked={this.state.check}
-                                        onClick={() => this.handleChange()}
-
+                                        onClick={() => this.handleChange(1)} // <-- Changed this line
                                         value='1'
                                     />
                                     {option1}
                                 </label>
+
                                 <label htmlFor='option-2'>
                                     <input
                                         type='checkbox'
                                         id='option-2'
-                                        value='2'
                                         checked={this.state.check}
-
-                                        onClick={() => {
-                                            this.handleChange()
-                                        }}
+                                        onClick={() => this.handleChange(2)} // <-- Changed this line
+                                        value='2'
                                     />
                                     {option2}
                                 </label>
+
                                 <label htmlFor='option-3'>
                                     <input
                                         type='checkbox'
                                         id='option-3'
-                                        value='2'
                                         checked={this.state.check}
-
-                                        onClick={() => {
-                                            this.handleChange()
-                                        }}
+                                        onClick={() => this.handleChange(3)} // <-- Changed this line
+                                        value='3'
                                     />
                                     {option3}
                                 </label>
+
                                 <label htmlFor='option-4'>
                                     <input
                                         type='checkbox'
                                         id='option-4'
-                                        value='4'
                                         checked={this.state.check}
-                                        onClick={() => {
-                                            this.handleChange()
-                                        }}
+                                        onClick={() => this.handleChange(4)} // <-- Changed this line
+                                        value='4'
                                     />
                                     {option4}
                                 </label>
@@ -109,7 +128,7 @@ class App extends Component {
                         </div>
                     </form>
                 </fieldset>
-                {!this.state.visible && <label><input type='submit' name='submit'/></label>}
+                {!this.state.visible && <h2>Your Recommendation: {this.state.recommendation}</h2>}
             </div>
         )
     }
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Form from './Form/Form.jsx'
-import Joke from './Joke/Joke.jsx'
-import PasswordV from "./PasswordV/PasswordV.jsx";
-import IPFinder from './IPFinder/IPFinder.jsx'
-import Dice from "./Dice/Dice.jsx";
-import AppG from "./GameRBS/AppG.jsx";
+
 import App from "./App.jsx"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/*<Form/>*/}
-    {/*<Joke/>*/}
-    {/*<PasswordV/>*/}
-    {/*<IPFinder/>*/}
-    {/*{<Dice/>}*/}
-    {/*<AppG/>*/}
       <App/>
   </StrictMode>,
 )
